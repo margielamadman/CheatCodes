866fa24b6a60db0cf8f1adb02ede67edb1e75801
mn <39255541+margielamadman@users.noreply.github.com>
2018-09-29 21:31:26 -0700

    Remove sudos and fix some errors

--- postinstall.sh
+++ postinstall.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
+#RUN with sudo
 
 #Update sources
-sudo apt-get update && sudo apt-get upgrade -y
+apt-get update && apt-get upgrade -y
 
 # Install packages
-sudo apt -y install build-essential git wget curl software-properties-common network-manager libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake i3 unclutter vim tmux git thunderbird neofetch fonts-noto fonts-inconsolata acpi compton fontconfig scrot htop tlp caca-utils atool mediainfo poppler-utils arandr feh dosfstools exfat-utils ffmpeg ntfs-3g pulsemixer unrar unzip zathura snapd pulseaudio
+apt -y install build-essential git wget curl software-properties-common ubuntu-drivers-common xorg xserver-xorg volumeicon-alsa network-manager libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake i3 unclutter vim tmux git thunderbird neofetch fonts-noto fonts-inconsolata acpi compton fontconfig scrot htop tlp caca-utils atool mediainfo poppler-utils arandr feh dosfstools exfat-utils ffmpeg ntfs-3g pulsemixer unrar unzip zathura snapd pulseaudio x11-xkb-utils mesa-utils mesa-utils-extra
 
 #Install snaps
-sudo snap install vlc
-sudo snap install brave
+snap install vlc
+snap install brave
 
 #Install st
 # clone the repository
+cd
 git clone https://github.com/margielamadman/ST-build.git st
 cd st
 
 # compile and install
-sudo make clean install
+make clean install
 cd
 
 #Install i3-gaps
@@ -33,29 +35,35 @@ mkdir -p build && cd build/
 # The prefix and sysconfdir are, obviously, dependent on the distribution.
 ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
 make
-sudo make install
+make install
 cd
  
 #Install fluxgui
-sudo add-apt-repository ppa:nathan-renniewaldock/flux
-sudo apt-get update
-sudo apt-get install fluxgui
+add-apt-repository ppa:nathan-renniewaldock/flux -y
+apt-get update
+apt-get -y install fluxgui
 
 #copy dotfiles
 git clone https://github.com/margielamadman/dotfiles.git
 cd dotfiles
-sudo cp .bash_aliases .bashrc wall.jpg .gitconfig .inputrc .profile .tmux.conf .vimrc .xinitrc ~/
-sudo cp unclutter /etc/default/
-sudo mkdir -p ~/.config/dunst/ && cp .config/dunst/dunstrc ~/.config/dunst/
-sudo mkdir -p ~/.config/fontconfig/ && cp .config/fontconfig/fonts.conf ~/.config/fontconfig/
-sudo mkdir -p ~/.config/htop/ && cp .config/htop/htoprc ~/.config/htop/
-sudo mkdir -p ~/.config/i3/ && cp .config/i3/config ~/.config/i3/
+cp .bash_aliases .bashrc wall.jpg .gitconfig .inputrc .profile .tmux.conf .vimrc .xinitrc ~/
+cp unclutter /etc/default/
+cp grub /etc/default/
+mkdir -p ~/.config/dunst/ && cp .config/dunst/dunstrc ~/.config/dunst/
+mkdir -p ~/.config/fontconfig/ && cp .config/fontconfig/fonts.conf ~/.config/fontconfig/
+mkdir -p ~/.config/htop/ && cp .config/htop/htoprc ~/.config/htop/
+mkdir -p ~/.config/i3/ && cp .config/i3/config ~/.config/i3/
+
+#update grub
+update-grub && update-grub2
 
 #create directories
 cd
-sudo mkdir Documents
-sudo mkdir Downloads
-sudo mkdir ~/Documents/repos
+mkdir Documents
+mkdir Downloads
+mkdir ~/Documents/repos
+mkdir ~/.ssh
+mkdir -p /media/usb
 
 #done
 cd
